bd8ab407e697da67eb730e27f9a1b6a9e936f54a
zhanglixiang <2490964714@qq.com>
2026-09-16 20:04:31 +0800

    1

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app=Flask(__name__)
 def index():
     return (render_template("index.html"))
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
     return (render_template("main.html"))
+
+@app.route("/transferMoney",methods=["GET","POST"])
+def transferMoney():
+    return (render_template("transferMoney.html"))
 
 if __name__ == "__main__":
     app.run()
